c50124e671ac4b2a9baa7ba9aa5da71e789e0ba7
Tanish Oberoi <tanishhacks@gmail.com>
2026-03-18 19:22:35 +0530

    Time: 1445 ms (8.55%), Space: 19.3 MB (51.65%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -2,6 +2,6 @@ class Solution:
     def mySqrt(self, x: int) -> int:
         i = 0
         while(True):
-            if i * i:
+            if i ** 2 > x:
                 return i - 1
             i += 1
